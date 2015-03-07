c33e67c2a0e13f40df8586be212e544cf20f33d4
Christian Hamm <christian.hamm@elekta.com>
2015-03-07 15:12:07 +0100

    Surface -> Texture, refactoring DrawGradient, use of malloc()

--- hero.c
+++ hero.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/mman.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_log.h>
 #include <SDL2/SDL_events.h>
@@ -25,13 +26,51 @@ const Uint32 DEPTH = 32;
 
 #define log_debug(...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
-void Hero_PutPixel(SDL_Surface *surface,
+static SDL_Texture* texture;
+static void* pixels;
+
+void Hero_PutPixel(void *pixels, int pitch,
     const Uint32 x, const Uint32 y, const Uint32 color) {
 
-    Uint8* pixel = (Uint8 *) surface->pixels;
-    pixel += (y * surface->pitch) + (x * sizeof(Uint32));
-    /*log_debug("x,y: %d,%d; pixel=%p", x, y, pixel);*/
+    Uint8* pixel = (Uint8 *) pixels;
+    pixel += (y * pitch) + (x * sizeof(Uint32));
     *((Uint32*) pixel) = color;
+}
+
+void Hero_ResizeTexture(SDL_Renderer* renderer, int width, int height) {
+    if(pixels)
+        pixels = realloc(pixels, width * height * 4);
+    else
+        pixels = malloc(width * height * 4);
+
+    memset(pixels, 0xff, width * height * 4);
+
+    if(texture)
+        SDL_DestroyTexture(texture);
+
+    texture = SDL_CreateTexture(renderer,
+                                         SDL_PIXELFORMAT_ARGB8888,
+                                         SDL_TEXTUREACCESS_STREAMING,
+                                         width,
+                                         height);
+}
+
+void Hero_DrawGradient(int window_w, int window_h, int frame_step) {
+        for (int y = 0; y < window_h ; y++) {
+            for (int x = 0; x < window_w ; x++) {
+                Uint32 number = y + frame_step;
+                //Uint32 number = rand();
+                Uint8 R = (x) % 256;
+                Uint8 G = (x) % 256;
+                Uint8 B = (number) % 256;
+                Uint32 color = (0x06 << 24)
+                                | ((B & (0xff)) << 16)
+                                | ((G & (0xff)) << 8)
+                                | ((R & 0xff));
+                Hero_PutPixel(pixels, window_w * 4,
+                    (x - frame_step) % window_w, y, color);
+            }
+        }
 }
 
 int main(int argc, char ** argv) {
@@ -75,16 +114,23 @@ int main(int argc, char ** argv) {
         WIDTH, HEIGHT,
         SDL_WINDOW_RESIZABLE);
 
-    SDL_Surface *screenSurface;
-    screenSurface = SDL_GetWindowSurface(window);
+
+    int window_w, window_h;
+    SDL_GetWindowSize(window, &window_w, &window_h);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+        //SDL_RENDERER_PRESENTVSYNC);
+         SDL_RENDERER_SOFTWARE);
 
     Uint32 frame_step = 0;
-
     int running = 1;
+
+    Hero_ResizeTexture(renderer, window_w, window_h);
 
     while(running) {
         Uint64 perf_freq = SDL_GetPerformanceFrequency();
         Uint64 perf_counter_start = SDL_GetPerformanceCounter();
+        SDL_GetWindowSize(window, &window_w, &window_h);
 
         SDL_Event event;
 
@@ -113,7 +159,7 @@ int main(int argc, char ** argv) {
                     SDL_Log("Window %d resized to %dx%d",
                             event.window.windowID, event.window.data1,
                             event.window.data2);
-                    screenSurface = SDL_GetWindowSurface(window);
+                    Hero_ResizeTexture(renderer, window_w, window_h);
                     break;
                 case SDL_WINDOWEVENT_MINIMIZED:
                     SDL_Log("Window %d minimized", event.window.windowID);
@@ -150,11 +196,11 @@ int main(int argc, char ** argv) {
             }
         }
 
-        // Direct access to surface->pixels
-        SDL_LockSurface(screenSurface);
+        //Hero_DrawGradient(window_w, window_h, frame_step);
 
-        for (int y = 0; y < screenSurface->h ; y++) {
-            for (int x = 0; x < screenSurface->w ; x++) {
+        /*
+        for (int y = 0; y < window_h ; y++) {
+            for (int x = 0; x < window_w ; x++) {
                 Uint32 number = y + frame_step;
                 //Uint32 number = rand();
                 Uint8 R = (x) % 256;
@@ -164,25 +210,40 @@ int main(int argc, char ** argv) {
                                 | ((B & (0xff)) << 16)
                                 | ((G & (0xff)) << 8)
                                 | ((R & 0xff));
-                Hero_PutPixel(screenSurface,
-                    (x - frame_step) % screenSurface->w, y, color);
+                Hero_PutPixel(pixels, window_w * 4,
+                    (x - frame_step) % window_w, y, color);
             }
         }
+        */
 
-        frame_step++;
+        SDL_RenderClear(renderer);
+        if (SDL_UpdateTexture(texture, 0, pixels, window_w * 4)) {
+            log_debug("Could not update texture!");
+            running = 0;
+        }
 
         // End direct access to pixels
-        SDL_UnlockSurface(screenSurface);
-        SDL_UpdateWindowSurface(window);
+        SDL_RenderCopy(renderer, texture, 0, 0);
+        SDL_RenderPresent(renderer);
+
+        // Performance
         Uint64 perf_counter_end = SDL_GetPerformanceCounter();
         Uint64 perf_counter_elapsed = perf_counter_end - perf_counter_start;
         double perf_per_frame = (((1000.0f * (double)perf_counter_elapsed) / (double)perf_freq));
 
         if((frame_step % 100) == 0)
             log_debug("Frame time %d: %f", frame_step, perf_per_frame);
+
+        frame_step++;
     }
 
-    SDL_FreeSurface(screenSurface);
+    if(pixels)
+        free(pixels);
+
+    if(texture)
+        SDL_DestroyTexture(texture);
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
